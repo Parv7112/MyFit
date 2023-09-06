440787cfdcd8355670d228da524d65bb511537b0
Parv Garg <parvg764@gmail.com>
2023-09-06 18:46:50 +0530

    some changes

--- src/routes.js
+++ src/routes.js
@@ -1,33 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Section1 from './components/Section1';
-import Section2 from './components/Section2';
-import Section3 from './components/Section3';
-import Section4 from './components/Section4';
-import Section5 from './components/Section5';
-import Section6 from './components/Section6';
-import Section7 from './components/Section7';
-import Section8 from './components/Section8';
-import Section9 from './components/Section9';
-import Section10 from './components/Section10';
-import Footer from './components/Footer';
+import LandingPage from './components/LandingPage';
 
 const App = () => {
     return (
         <Router>
-            <Navbar />
-            <Section1 />
-            <Section2 />
-            <Section3 />
-            <Section4 />
-            <Section5 />
-            <Section6 />
-            <Section7 />
-            <Section8 />
-            <Section9 />
-            <Section10 />
-            <Footer />
+            <LandingPage />
         </Router>
     );
 };
