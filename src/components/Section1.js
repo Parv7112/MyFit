24faa38ed14e9b3624f5601f73573a0971796a30
Parv Garg <parvg764@gmail.com>
2023-09-06 17:02:35 +0530

    some changes

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -5,10 +5,12 @@ const Section1 = () => {
         <div className="container-fluid overflow-hidden">
             <div className="row bg-black text-white">
                 <div className="col-md-6 my-5 py-md-5 order-md-1 order-2">
+                    
                     <h1 className='fs-1 fw-bold text-left my-md-5 mx-md-5 px-md-5'>
                         The #1 coaching app to <br />
                         better engage your clients.
                     </h1>
+
                     <p className='mx-md-5 px-md-5 my-md-5'>
                         Take your coaching online and deliver an experience like nothing <br />
                         your clients have ever seen before. With fitness, nutrition, and habit- <br />
@@ -16,16 +18,17 @@ const Section1 = () => {
                         more — it's everything you need to motivate and inspire. <br />
                     </p>
 
-                    <button className="btn btn-outline-light text-white fw-bold section1-button1">
-                        <span className="">
-                            Login
-                        </span>
-                    </button>
-                    <button className="btn btn-outline-light text-white fw-bold section1-button2">
-                        <span className="">
-                            Get Started
-                        </span>
-                    </button> 
+                        <button className="btn btn-outline-light text-white fw-bold section1-button1">
+                            <span>
+                                Login
+                            </span>
+                        </button>
+                        <button className="btn btn-outline-light text-white fw-bold section1-button2">
+                            <span>
+                                Get Started
+                            </span>
+                        </button>
+
                 </div>
 
                 <div className="col-md-6 px-0 order-md-2 order-1">
